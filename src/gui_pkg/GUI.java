6d940ab1ee14712c95c1781ecafd1cd37f29f1c6
Evan Carter <32000073+Plaligraphy@users.noreply.github.com>
2020-03-02 10:03:37 -0800

    added gui features

--- src/gui_pkg/GUI.java
+++ src/gui_pkg/GUI.java
@@ -1,12 +1,14 @@
 package gui_pkg;
-
+//GUI Portion of Evan's Brainfuck compiler (March 1st, 2020)
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
 public class GUI {
+    pkg.FileHandler fh = new pkg.FileHandler();
+    pkg.LogicHandler lh = new pkg.LogicHandler();
+
     public void init() {
-        createFrame(400,400,0,"Test");
+        createFrame(400,400,0,"BrainFuck Compiler");
     }
     private void createFrame(int x, int y, int opt, String title) {
         JFrame f = new JFrame(title);
@@ -16,9 +18,17 @@ public class GUI {
         JPanel c = new JPanel();m.add(c,BorderLayout.CENTER);
         switch(opt) {
             case 0:
-                JLabel title_ = new JLabel("title");
+                JLabel title_ = new JLabel("BrainFuck Compiler");
+                JLabel creator = new JLabel("by: Evan Carter");
 
-                c.add(title_);
+                JButton exit = new JButton("Exit"); exit.addActionListener(e -> System.exit(0));
+                creator.setFont(new Font("Times Roman", Font.PLAIN, 10));
+
+
+                n.add(title_);
+                n.add(creator);
+
+                s.add(exit);
                 break;
             case 1:
                 break;
@@ -30,3 +40,11 @@ public class GUI {
         f.setLocationRelativeTo(null);
     }
 }
+
+
+
+
+
+
+
+/// hows ashley buddy?
